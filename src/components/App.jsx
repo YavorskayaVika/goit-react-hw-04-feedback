ed076f855b947f57066ae4da9de9aa59d96c8042
YavorskayaVika <135236285+YavorskayaVika@users.noreply.github.com>
2023-09-25 12:11:45 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,71 @@
-import { FeedBack } from "./Feedback/Feedback";
-import React from "react";
-import { StyledBox } from "./App.styled";
+import { FeedBack } from './FeedBack/FeedBackOptions';
+import React, { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
+import { StyledBox } from './App.styled';
 
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onLeaveFeedback = name => {
+  const onLeaveFeedback = name => {
     switch (name) {
       case 'good':
-        this.setState(prev => ({ good: prev.good + 1 }));
+        setGood(prev => prev + 1);
         break;
 
       case 'neutral':
-        this.setState(prev => ({ neutral: prev.neutral + 1 }));
+        setNeutral(prev => prev + 1);
         break;
 
       case 'bad':
-        this.setState(prev => ({ bad: prev.bad + 1 }));
+        setBad(prev => prev + 1);
         break;
       default:
         console.log('Invalid btn');
     }
   };
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
-  countPositiveFeedbackPercentage = () => {
-    const totalFeedBack = this.countTotalFeedback();
-    if (totalFeedBack === 0) {
-      return 0;
-    }
-    return Math.floor((this.state.good / totalFeedBack) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    const totalFeedBack = countTotalFeedback();
+
+    return totalFeedBack === 0 ? 0 : Math.ceil((good / totalFeedBack) * 100);
   };
-  render() {
-    const totalPositivePerc = this.countPositiveFeedbackPercentage();
-    const totalFeedback = this.countTotalFeedback();
-    const { good, neutral, bad } = this.state;
-    const feedback = ['good', 'neutral', 'bad'];
-    const statsValue = totalFeedback > 0;
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <StyledBox>
-          <Section title="Please leave feedback">
-            <FeedBack
-              onLeaveFeedback={this.onLeaveFeedback}
-              options={feedback}
+  const feedback = ['good', 'neutral', 'bad'];
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <StyledBox>
+        <Section title="Please leave feedback">
+          <FeedBack onLeaveFeedback={onLeaveFeedback} options={feedback} />
+        </Section>
+        <Section title="Statistics">
+          {countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
             />
-          </Section>
-          <Section title="Statistics">
-            {statsValue ? (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={totalFeedback}
-                positivePercentage={totalPositivePerc}
-              />
-            ) : (
-              <Notification message="There is no feedback" />
-            )}
-          </Section>
-        </StyledBox>
-      </div>
-    );
-  }
-}
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </StyledBox>
+    </div>
+  );
+};
